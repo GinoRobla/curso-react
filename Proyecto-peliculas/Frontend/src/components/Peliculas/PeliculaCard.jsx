97f7f879627b2f02ddc9818d2bc11e35418f37c6
gino robla <ginorobla2004@gmail.com>
2025-08-18 23:36:30 -0300

    Eliminar Proyecto-peliculas del repositorio

--- Proyecto-peliculas/Frontend/src/components/Peliculas/PeliculaCard.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const PeliculaCard = ({ titulo, descripcion }) => (
-    <div className="movie">
-        <h3 className="movie-title">{titulo}</h3>
-        <p>{descripcion}</p>
-    </div>
-);
